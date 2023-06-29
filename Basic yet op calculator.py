93e62c4d2cf29e16ec734d73e9ad351ff6c090ea
PythonWhizKid <138038178+PythonWhizKid@users.noreply.github.com>
2023-06-29 13:05:01 +0530

    Create Basic yet op calculator.py

---
+++ Basic yet op calculator.py
@@ -0,0 +1,20 @@
+#Let us make a basic calculator!
+from time import sleep
+print("Chalo bhai karte hai calculate...")
+#sleep(1)
+x, a, y, b = float(input("Enter first number ")), input("Write first operator '+,-,*,/' "), float(input("Enter second number ")), input("Write second operator or type 'r' for result ")
+#system for third number
+if b == "r" and a == "+":
+    R = float(x + y)
+    print("Answer: ",R)
+elif b == "r" and a == "-":
+    R = float(x - y)
+    print("Answer: ",R)
+elif b == "r" and a == "*":
+    R = float(x * y)
+    print("Answer: ",R)
+elif b == "r" and a == "/":
+    R = float(x / y)
+    print("Answer: ",R)
+elif b == "r":
+    print("Write operator carefully...")

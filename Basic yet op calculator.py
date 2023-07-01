f535ab5ea70baca9bced75594fbb7f13fb66dd4b
PythonWhizKid <138038178+PythonWhizKid@users.noreply.github.com>
2023-07-01 15:39:40 +0530

    Update BasicCalculator.py

--- Basic yet op calculator.py
+++ Basic yet op calculator.py
@@ -2,20 +2,92 @@
 #This is still under working!!
 from time import sleep
 print("Chalo bhai karte hai calculate...")
-#sleep(1)
+sleep(1)
 x, a, y, b = float(input("Enter first number ")), input("Write first operator '+,-,*,/' "), float(input("Enter second number ")), input("Write second operator or type 'r' for result ")
-#system for third number
-if b == "r" and a == "+":
+
+if a == "+":
     R = float(x + y)
-    print("Answer: ",R)
-elif b == "r" and a == "-":
+    if b == "r":
+        print("Answer: ",R)
+    #system for third number
+    if b == "+":
+        z = float(input("Write third number: "))
+        r = float(x + y + z)
+        print("Answer: ",r)
+    elif b == "-":
+        z = float(input("Write third number: "))
+        r = float(x + y - z)
+        print("Answer: ",r)
+    elif b == "*":
+        z = float(input("Write third number: "))
+        r = float(x + y * z)
+        print("Answer: ",r)
+    elif b == "/":
+        z = float(input("Write third number: "))
+        r = float(x + y / z)
+        print("Answer: ",r)
+elif a == "-":
     R = float(x - y)
-    print("Answer: ",R)
-elif b == "r" and a == "*":
+    if b == "r":
+        print("Answer: ",R)
+    #system for third number
+    if b == "+":
+        z = float(input("Write third number: "))
+        r = float(x - y + z)
+        print("Answer: ",r)
+    elif b == "-":
+        z = float(input("Write third number: "))
+        r = float(x - y - z)
+        print("Answer: ",r)
+    elif b == "*":
+        z = float(input("Write third number: "))
+        r = float(x - y * z)
+        print("Answer: ",r)
+    elif b == "/":
+        z = float(input("Write third number: "))
+        r = float(x - y / z)
+        print("Answer: ",r)
+elif a == "*":
     R = float(x * y)
-    print("Answer: ",R)
-elif b == "r" and a == "/":
+    if b == "r":
+        print("Answer: ",R)
+    #system for third number
+    if b == "+":
+        z = float(input("Write third number: "))
+        r = float(x * y + z)
+        print("Answer: ",r)
+    elif b == "-":
+        z = float(input("Write third number: "))
+        r = float(x * y - z)
+        print("Answer: ",r)
+    elif b == "*":
+        z = float(input("Write third number: "))
+        r = float(x * y * z)
+        print("Answer: ",r)
+    elif b == "/":
+        z = float(input("Write third number: "))
+        r = float(x * y / z)
+        print("Answer: ",r)
+elif a == "/":
     R = float(x / y)
-    print("Answer: ",R)
+    if b == "r":
+        print("Answer: ",R)
+    #system for third number
+    if b == "+":
+        z = float(input("Write third number: "))
+        r = float(x / y + z)
+        print("Answer: ",r)
+    elif b == "-":
+        z = float(input("Write third number: "))
+        r = float(x / y - z)
+        print("Answer: ",r)
+    elif b == "*":
+        z = float(input("Write third number: "))
+        r = float(x / y * z)
+        print("Answer: ",r)
+    elif b == "/":
+        z = float(input("Write third number: "))
+        r = float(x / y / z)
+        print("Answer: ",r)
 elif b == "r":
     print("Write operator carefully...")

b949cbefa0999182c002d76d130af43b8e973958
PythonWhizKid <138038178+PythonWhizKid@users.noreply.github.com>
2023-07-01 16:28:37 +0530

    Update calculator.py
    Added loop

--- Basic yet op calculator.py
+++ Basic yet op calculator.py
@@ -3,91 +3,120 @@
 from time import sleep
 print("Chalo bhai karte hai calculate...")
 sleep(1)
-x, a, y, b = float(input("Enter first number ")), input("Write first operator '+,-,*,/' "), float(input("Enter second number ")), input("Write second operator or type 'r' for result ")
 
-if a == "+":
-    R = float(x + y)
-    if b == "r":
-        print("Answer: ",R)
+while True:
+    x, a, y, b = float(input("Enter first number ")), input("Write first operator '+,-,*,/' "), float(input("Enter second number ")), input("Write second operator or type 'r' for result ")
+    
+    if a == "+":
+        R = float(x + y)
+        if b == "r":
+            print("Answer: ",R)
+            cont = input("Press any key to continue...")
+        
     #system for third number
-    if b == "+":
-        z = float(input("Write third number: "))
-        r = float(x + y + z)
-        print("Answer: ",r)
-    elif b == "-":
-        z = float(input("Write third number: "))
-        r = float(x + y - z)
-        print("Answer: ",r)
-    elif b == "*":
-        z = float(input("Write third number: "))
-        r = float(x + y * z)
-        print("Answer: ",r)
-    elif b == "/":
-        z = float(input("Write third number: "))
-        r = float(x + y / z)
-        print("Answer: ",r)
-elif a == "-":
-    R = float(x - y)
-    if b == "r":
-        print("Answer: ",R)
+        if b == "+":
+            z = float(input("Write third number: "))
+            r = float(x + y + z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+            
+        elif b == "-":
+            z = float(input("Write third number: "))
+            r = float(x + y - z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+        elif b == "*":
+            z = float(input("Write third number: "))
+            r = float(x + y * z)
+            print("Answer: ",r)
+        elif b == "/":
+            z = float(input("Write third number: "))
+            r = float(x + y / z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+        continue
+    elif a == "-":
+        R = float(x - y)
+        if b == "r":
+            print("Answer: ",R)
+            cont = input("Press any key to continue...")
     #system for third number
-    if b == "+":
-        z = float(input("Write third number: "))
-        r = float(x - y + z)
-        print("Answer: ",r)
-    elif b == "-":
-        z = float(input("Write third number: "))
-        r = float(x - y - z)
-        print("Answer: ",r)
-    elif b == "*":
-        z = float(input("Write third number: "))
-        r = float(x - y * z)
-        print("Answer: ",r)
-    elif b == "/":
-        z = float(input("Write third number: "))
-        r = float(x - y / z)
-        print("Answer: ",r)
-elif a == "*":
-    R = float(x * y)
-    if b == "r":
-        print("Answer: ",R)
+        if b == "+":
+            z = float(input("Write third number: "))
+            r = float(x - y + z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+        elif b == "-":
+            z = float(input("Write third number: "))
+            r = float(x - y - z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+        elif b == "*":
+            z = float(input("Write third number: "))
+            r = float(x - y * z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+        elif b == "/":
+            z = float(input("Write third number: "))
+            r = float(x - y / z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+        continue
+    elif a == "*":
+        R = float(x * y)
+        if b == "r":
+            print("Answer: ",R)
+            cont = input("Press any key to continue...")
     #system for third number
-    if b == "+":
-        z = float(input("Write third number: "))
-        r = float(x * y + z)
-        print("Answer: ",r)
-    elif b == "-":
-        z = float(input("Write third number: "))
-        r = float(x * y - z)
-        print("Answer: ",r)
-    elif b == "*":
-        z = float(input("Write third number: "))
-        r = float(x * y * z)
-        print("Answer: ",r)
-    elif b == "/":
-        z = float(input("Write third number: "))
-        r = float(x * y / z)
-        print("Answer: ",r)
-elif a == "/":
-    R = float(x / y)
-    if b == "r":
-        print("Answer: ",R)
+        if b == "+":
+            z = float(input("Write third number: "))
+            r = float(x * y + z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+        elif b == "-":
+            z = float(input("Write third number: "))
+            r = float(x * y - z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+        elif b == "*":
+            z = float(input("Write third number: "))
+            r = float(x * y * z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+        elif b == "/":
+            z = float(input("Write third number: "))
+            r = float(x * y / z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+        continue
+    elif a == "/":
+        R = float(x / y)
+        if b == "r":
+            print("Answer: ",R)
+            cont = input("Press any key to continue...")
     #system for third number
-    if b == "+":
-        z = float(input("Write third number: "))
-        r = float(x / y + z)
-        print("Answer: ",r)
-    elif b == "-":
-        z = float(input("Write third number: "))
-        r = float(x / y - z)
-        print("Answer: ",r)
-    elif b == "*":
-        z = float(input("Write third number: "))
-        r = float(x / y * z)
-        print("Answer: ",r)
-    elif b == "/":
-        z = float(input("Write third number: "))
-        r = float(x / y / z)
-        print("Answer: ",r)
-elif b == "r":
-    print("Write operator carefully...")
+        if b == "+":
+            z = float(input("Write third number: "))
+            r = float(x / y + z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+        elif b == "-":
+            z = float(input("Write third number: "))
+            r = float(x / y - z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+        elif b == "*":
+            z = float(input("Write third number: "))
+            r = float(x / y * z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+        elif b == "/":
+            z = float(input("Write third number: "))
+            r = float(x / y / z)
+            print("Answer: ",r)
+            cont = input("Press any key to continue...")
+        continue
+    elif b == "r":
+        print("Write operator carefully...")
+        cont = input("Press any key to continue...")
+        continue
